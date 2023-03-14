fb30f089e16ed3debef81063b493597322b34f5c
Vini Pereira <vini@saltandlime.com.au>
2023-03-15 09:59:04 +1100

    log nowUnix and sydney_todayUnix

--- getUnixDate.js
+++ getUnixDate.js
@@ -7,4 +7,7 @@ const sydney_today = `${sydney_now.slice(0,9)} 00:00:00 ${sydney_now.slice(-6)}`
 // get UNIX TIMESTAMP of current DATE in local timezone
 const sydney_todayUnix = Math.floor(new Date(sydney_today).getTime() / 1000);
 
-console.log(sydney_now, sydney_today, sydney_todayUnix);
+// get UNIX TIMESTAMP of NOW in user's timezone
+const nowUnix = Math.floor(new Date().getTime() / 1000);
+
+console.log(nowUnix, sydney_todayUnix);
