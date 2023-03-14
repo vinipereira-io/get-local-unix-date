69e45768ed17dc966b103221afe80ab8165e48e0
Vini Pereira <vini@saltandlime.com.au>
2023-03-15 10:21:05 +1100

    add unix in ms

--- getUnixDate.js
+++ getUnixDate.js
@@ -4,10 +4,16 @@ const sydney_now = new Date().toLocaleString("en-US", { timeZone: "Australia/Syd
 // get current DATE in local timezone
 const sydney_today = `${sydney_now.slice(0,9)} 00:00:00 ${sydney_now.slice(-6)}`;
 
-// get UNIX TIMESTAMP of current DATE in local timezone
-const sydney_todayUnix = Math.floor(new Date(sydney_today).getTime() / 1000);
+// get UNIX TIMESTAMP (seconds) of current DATE in local timezone
+const sydney_todayUnix_s = Math.floor(new Date(sydney_today).getTime() / 1000);
 
-// get UNIX TIMESTAMP of NOW in user's timezone
-const nowUnix = Math.floor(new Date().getTime() / 1000);
+// get UNIX TIMESTAMP (seconds) of NOW in user's timezone
+const nowUnix_s = Math.floor(new Date().getTime() / 1000);
 
-console.log(nowUnix, sydney_todayUnix);
+// get UNIX TIMESTAMP (ms) of current DATE in local timezone
+const sydney_todayUnix_ms = new Date(sydney_today).getTime();
+
+// get UNIX TIMESTAMP (ms) of NOW in user's timezone
+const nowUnix_ms = new Date().getTime();
+
+console.log(nowUnix_ms, sydney_todayUnix_ms);
